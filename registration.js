beb617c8cb30bbab9262dc07eac856d92c523a59
Fuad Khan <fuadkhanantor1@gmail.com>
2023-09-12 17:09:24 +0600

    registration functionality added

--- registration.js
+++ registration.js
@@ -1,11 +1,26 @@
-document.getElementById("registrationForm").addEventListener("submit", function (e) {
+document
+  .getElementById("registrationForm")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    // You can add your registration logic here (e.g., send data to a server).
+    // Check if the email is already registered
+    const registeredUsers =
+      JSON.parse(localStorage.getItem("registeredUsers")) || [];
+    const isEmailRegistered = registeredUsers.some(
+      (user) => user.email === email
+    );
+    if (isEmailRegistered) {
+      alert("Email Already Registered ! Please log in");
+      return;
+    }
+    // Register the user (store in local storage)
+    const user = { name, email, password };
+    registeredUsers.push(user);
+    localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
 
     // Redirect to login page after registration
     window.location.href = "login.html";
-});
+  });
