062382c48054a23fb6d9db7ef28d5061baeb734b
Fuad Khan <fuadkhanantor1@gmail.com>
2023-09-13 02:29:36 +0600

    create task functionality added

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,14 @@
+  // Authentication 
 const isAuthenticated = localStorage.getItem("authenticated") === "true";
 
 const userId = localStorage.getItem("userId");
 
-
-// Retrieve the user's profile data using the email
 const userProfile = JSON.parse(localStorage.getItem(userId));
 
 const userNameElement = document.getElementById("userName");
 
 if (userProfile && userNameElement) {
-    userNameElement.textContent = userProfile.name;
+  userNameElement.textContent = userProfile.name;
 }
 
 if (!isAuthenticated) {
@@ -18,3 +17,77 @@ if (!isAuthenticated) {
 document.getElementById("logoutLink").addEventListener("click", () => {
   localStorage.setItem("authenticated", "false");
 });
+
+// ///////   Task Form   /////////////////////////////////
+const createTaskForm = document.getElementById("createTaskForm");
+createTaskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Get task data from the form
+  const taskTitle = createTaskForm.querySelector("#taskTitle").value;
+  const taskDescription =
+    createTaskForm.querySelector("#taskDescription").value;
+  const dueDate = createTaskForm.querySelector("#dueDate").value;
+  const priority = createTaskForm.querySelector("#priority").value;
+  const assignee = createTaskForm.querySelector("#assignee").value;
+
+  // Create a task object
+  const task = {
+    title: taskTitle,
+    description: taskDescription,
+    dueDate: dueDate,
+    priority: priority,
+    assignee: assignee,
+    status: "pending",
+  };
+
+  const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+  taskList.push(task);
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+
+  createTaskForm.reset();
+
+  updateTaskList(taskList);
+});
+
+// Function updateTaskList
+function updateTaskList(tasks) {
+  const taskListDisplay = document.getElementById("taskList");
+  taskListDisplay.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const taskItem = document.createElement("div");
+    taskItem.className =
+      "mb-4 p-4 border rounded flex justify-between items-center";
+
+    const taskTitleElement = document.createElement("h3");
+    taskTitleElement.textContent = task.title;
+
+    const taskDescriptionElement = document.createElement("p");
+    taskDescriptionElement.textContent = task.description;
+
+    const dueDateElement = document.createElement("p");
+    dueDateElement.textContent = "Due Date: " + task.dueDate;
+
+    const priorityElement = document.createElement("p");
+    priorityElement.textContent = "Priority: " + task.priority;
+
+    const assigneeElement = document.createElement("p");
+    assigneeElement.textContent = "Assignee: " + task.assignee;
+
+    // Create task status element
+    const taskStatusElement = document.createElement("p");
+    taskStatusElement.textContent = "Status: " + task.status;
+
+    taskItem.appendChild(taskTitleElement);
+    taskItem.appendChild(taskDescriptionElement);
+    taskItem.appendChild(dueDateElement);
+    taskItem.appendChild(priorityElement);
+    taskItem.appendChild(assigneeElement);
+    taskItem.appendChild(taskStatusElement);
+
+    taskListDisplay.appendChild(taskItem);
+  });
+}
+
+const initialTaskList = JSON.parse(localStorage.getItem("taskList")) || [];
+updateTaskList(initialTaskList);
